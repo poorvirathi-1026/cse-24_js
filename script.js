b7883968f707a5fd1ba9b58f5c47dfb0c35d8034
Poorvi Rathi <poorvirathi05@gmail.com>
2026-02-24 16:25:09 +0530

    commit

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
-console.log("hello world")
-document.write("hello world")
+//console.log("hello world")
+//document.write("hello world")
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+
+
+// function/
+var sayhello=function()
+
+{
+    alert('hello world!');
+
+}
+
+
+
+//array
+// var numberarray=[1,2,3] //static array
+// var animals =new array("cat,dog")
+
+
+//js operators
+//=:assign the value
+//==:compare the value
+//===:strictly check date type and values both
+
+//maximum out of three no. by using ternary operator
+
+///loop
+// var count;
+// document.write("starting loop"+"<br/>")
+// for(count=0;count<10;count++);
